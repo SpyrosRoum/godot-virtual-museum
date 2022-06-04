58b4cbc082f93b7d9f9e5f68107912457d60d699
MineBill <minebill@protonmail.com>
2022-06-04 16:01:26 +0300

    Added ability to get and show info from exhibits

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -8,20 +8,41 @@ export var mouse_sensitivity: float = 0.1
 
 export(NodePath) onready var ui_manager = get_node(ui_manager) as UIManager
 
+onready var ray = $Head/Camera/RayCast
 var velocity: Vector3 = Vector3.ZERO
+
+var active_exhibit: Exhibit
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float) -> void:
-	movement(delta)
+	check_exhibit()
+	if !ui_manager.active_info():
+		movement(delta)
 	ui_manager.set_minimap(get_node("Viewport").get_texture())
+
+func check_exhibit() -> void:
+	if ray.is_colliding():
+		var obj = ray.get_collider()
+		if active_exhibit == null and obj is Exhibit:
+			active_exhibit = obj as Exhibit
+			ui_manager.spawn_prompt("Interact with %s" % [active_exhibit.exhibit_data.name])
+	elif active_exhibit != null:
+		active_exhibit = null
+		ui_manager.destroy_prompt()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		mouse_look(event.relative)
+		if !ui_manager.active_info():
+			mouse_look(event.relative)
 	elif event.is_action_pressed("toggle_cursor"):
 		toggle_mouse()
+	elif event.is_action_pressed("interact"):
+		if active_exhibit != null and !ui_manager.active_info():
+			var data = active_exhibit.exhibit_data
+			ui_manager.spawn_info(data.name, data.description)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func mouse_look(mouse_movement: Vector2) -> void:
 	rotate_y(-deg2rad(mouse_movement.x * mouse_sensitivity))
@@ -41,7 +62,7 @@ func get_input_vector() -> Vector3:
 	return Vector3(i.x, 0, i.y)
 
 func toggle_mouse() -> void:
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
---
+++ Scripts/ExhibitResource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name ExhibitData
+
+export var name: String
+export(String, MULTILINE) var description: String
